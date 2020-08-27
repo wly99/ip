10e7a59f6b9a59875bcc9f0a54e1ec17e78a99b5
wly99 <wang1426@e.ntu.edu.sg>
2020-08-27 22:07:19 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+
+        int MAX_SIZE = 100;
+        String[] data = new String[MAX_SIZE];
+        int idx = 0;
+        String[] temp;
+        int i;
 
         String greet = "____________________________________________________________\n"
                 + "Hello! I'm Duke\n"
@@ -14,7 +21,19 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         while (!(input.equals("bye"))){
-            System.out.println(input);
+            switch (input){
+                case "list":
+                    System.out.println("Here's what you have in the list:");
+                    for (i=0; i<idx; i++){
+                        System.out.println((i+1) + ". " + data[i]);
+                    }
+                    break;
+                default:
+                    data[idx] = input;
+                    idx++;
+                    System.out.println("    I added '" + input + "' to the list");
+                    break;
+            }
             input = in.nextLine();
         }
         System.out.println(goodbye);
