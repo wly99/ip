cacc6a279ff25a2e341efd2ca8bbf41c7b67aded
wly99 <wang1426@e.ntu.edu.sg>
2020-08-28 10:33:01 +0800

    A-CodingStandard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -22,21 +21,19 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        while (!(input.equals("bye"))){
+        while (!(input.equals("bye"))) {
             if (input.contains("list")) {
                 System.out.println("Here's what you have in the list:");
                 for (i = 0; i < idx; i++) {
                     System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
-            }
-            else if (input.contains("done")){
+            } else if (input.contains("done")) {
                 done_temp = input.split(" ")[1];
                 done_idx = Integer.parseInt(done_temp);
                 tasks[done_idx-1].markTaskAsDone();
                 System.out.println("    Nice! I've marked this task as done:");
                 System.out.println(("  [" + tasks[done_idx-1].getStatusIcon() + "] " + tasks[done_idx-1].getDescription()));
-            }
-            else{
+            } else {
                 tasks[idx] = new Task(input);
                 idx++;
                 System.out.println("    I added '" + input + "' to the list");
